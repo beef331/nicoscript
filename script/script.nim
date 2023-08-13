0b0a1b8cd97febeae3875e22d040b76c5a5bc21c
Jason Beetham <beefers331@gmail.com>
2023-08-12 20:29:24 -0600

    The editor certainly does work it seems

--- script/script.nim
+++ script/script.nim
@@ -1,5 +1,5 @@
 import nicoscript
-import std/[math, strutils]
+import std/[math, strutils, tables]
 
 type 
   Cursor = object
@@ -9,6 +9,32 @@ type
     cursor: Cursor
     lines: seq[string]
     screenPos: int
+
+const
+  declColour = 2
+  methodColour = 4
+  keywordColour = 12
+  otherColour = 9
+  callColour = 14
+  numberColour = 3
+  lineColour = 12
+
+  colours = {
+    "var": declColour,
+    "let": declColour,
+    "const": declColour,
+    "proc": methodColour,
+    "func": methodColour,
+    "template": methodColour,
+    "macro": methodColour,
+    "if": keywordColour,
+    "of": keywordColour,
+    "else": keywordColour,
+    "when": keywordColour,
+    "elif": keywordColour,
+    "case": keywordColour,
+    "for": keywordColour
+  }.toTable
 
 
 var 
@@ -30,14 +56,12 @@ proc update(dt: float32) =
   
     if btnpr(pcLeft, 3):
       dec editor.cursor.column
-
     if btnpr(pcRight, 3):
       inc editor.cursor.column
     
-    if btnpr(pcDown, 3):
+    if btnpr(pcDown, 2):
       inc editor.cursor.line
-
-    if btnpr(pcUp, 3):
+    if btnpr(pcUp, 2):
       dec editor.cursor.line
 
     if btnpr(pcBack, 5):
@@ -72,23 +96,67 @@ proc update(dt: float32) =
 
 proc draw() =
   cls()
+
+  let
+    digits = 4 
+    startX = digits * textWidth(" ") + 5
+
   for ind in editor.screenPos .. min(editor.screenPos + rows(), editor.lines.high):
-    let y = ind - editor.screenPos
-    setColor(3)
-    print($ind, 1, y * (fontHeight() + 1))
-    setColor(8)
-    print(editor.lines[ind], 10, y * (fontHeight() + 1))
+    let line = editor.lines[ind]
+    var x = startX 
+    let y = (ind - editor.screenPos) * (fontHeight() + 1)
+    setColor(lineColour)
+    print($ind, 1, y)
+    for (tok, isSep)in line.tokenize:
+      if not isSep:
+        if tok in colours:
+          setColor(colours[tok])
+        elif tok.allCharsInSet(Digits):
+          setColor(numberColour)
+        else:
+          setColor(otherColour)
+        let ind = tok.find'('
+        if ind >= 0:
+          setColor(callColour)
+          let left = tok[0..ind - 1]
+          print(left, x, y)
+          x += textWidth(left)
+          if ind < tok.len:
+            setColor(otherColour)
+            let right  = tok[ind..^1]
+            print(right, x, y)
+            x += textWidth(right)
+        else:
+          print(tok, x, y)
+          x += textWidth(tok)
+      else:
+        x += textWidth(tok)
+
   setColor(7)
-  print("_", 10 + editor.cursor.column * 4, (editor.cursor.line - editor.screenPos) * (fontHeight() + 1) + 1)
+  print("_", startX + editor.cursor.column * textWidth(" "), (editor.cursor.line - editor.screenPos) * (fontHeight() + 1) + 1)
 
 proc init =
   startTextInput()
   editor.lines = readScript().splitLines
-  setTargetSize(512, 128)
+  setTargetSize(256, 128)
 
 init("appname", "orgname")
-createWindow("hmm", 256, 256, 5, false)
+createWindow("hmm", 256, 256, 4, false)
 run(init, update, draw)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
